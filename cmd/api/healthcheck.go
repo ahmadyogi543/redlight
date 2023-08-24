faa39b57856ee842dd4c86b78aa6d7f9eb57997a
Ahmad Yogi <ahmadyogi543@gmail.com>
2023-08-24 08:54:48 +0800

    chore: add error response utils

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -13,7 +13,6 @@ func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
