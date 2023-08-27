860b51a6eb73868db1a910b11c341e366df4a478
Ahmad Yogi <ahmadyogi543@gmail.com>
2023-08-27 09:26:04 +0800

    chore: add mock for MovieModel

--- internal/data/models.go
+++ internal/data/models.go
@@ -9,12 +9,25 @@ var (
 	ErrRecordNotFound = errors.New("record not found")
 )
 
+type MovieModelInterface interface {
+	Insert(movie *Movie) error
+	Get(id int64) (*Movie, error)
+	Update(movie *Movie) error
+	Delete(id int64) error
+}
+
 type Models struct {
-	Movies MovieModel
+	Movies MovieModelInterface
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+	}
+}
+
+func NewMockModels() Models {
+	return Models{
+		Movies: MockMovieModel{},
 	}
 }
